5dec6e96f642b36f5ac440b181bb2e667e60747e
Lalatendu Bhatta <lalatendubhatta@gmail.com>
2025-01-06 13:11:21 +0530

    first commit

---
+++ routes/admin.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const adminRouter = Router()
+
+//api
+
+//demo
+adminRouter.get("/", (req, res) => res.send("admin router is working"))
+
+
+export default adminRouter
